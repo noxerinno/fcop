ab7029152810bbf72b11dfd536506b55b8c7cfe7
Noxerinno <frantz.darbon@telecomnancy.net>
2025-04-14 19:54:34 +0200

    [FCOP] Blinding lights Make the LED blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+# define LED 2
 
-// put function declarations here:
-int myFunction(int, int);
+// // put function declarations here:
+// int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+
+  Serial.begin(115200);  // Initialize serial communication
+  pinMode(LED, OUTPUT);
+  Serial.println("Starting LED Blink...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(LED, HIGH);
+  Serial.println("LED ON");
+  delay(2000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED OFF");
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
