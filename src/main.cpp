a36edb03de2e670095404a845d657aef1ae186d0
Noxerinno <frantz.darbon@telecomnancy.net>
2025-04-15 17:45:07 +0200

    [FCOP] Hello, World! Display 'Hello, World!' on the OLED display

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,35 @@
 #include <Arduino.h>
-# define LED 2
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// // put function declarations here:
-// int myFunction(int, int);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+// Adresse I2C de l'écran (0x3C est le plus courant)
+#define OLED_ADDRESS 0x3C
+
+// Création de l'objet écran (avec interface I2C)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
+	Serial.begin(115200);
 
-  Serial.begin(115200);  // Initialize serial communication
-  pinMode(LED, OUTPUT);
-  Serial.println("Starting LED Blink...");
+	// Initialisation de l'écran
+	if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
+    	Serial.println(F("Échec de l'initialisation de l'écran OLED"));
+   		while (true); // Stop si échec
+  	}
+
+	display.clearDisplay();
+
+	display.setTextSize(2);             // Taille du texte
+	display.setTextColor(SSD1306_WHITE); // Couleur du texte
+	display.setCursor(0, 0);             // Position du texte
+	display.println(F("Hello, World!"));        // Texte à afficher
+	display.display();                   // Rafraîchit l'écran
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  digitalWrite(LED, HIGH);
-  Serial.println("LED ON");
-  delay(2000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED OFF");
-  delay(2000);
 }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
