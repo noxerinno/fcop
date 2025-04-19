afcb1506f416dea7c0a29b451ea9b4137d554630
Noxerinno <frantz.darbon@telecomnancy.net>
2025-04-19 19:44:44 +0200

    [FCOP] And there was light Display luminosity data in percent from LDR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,27 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDRESS 0x3C		// I2C screen address
+#define OLED_RESET -1
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);		// Creating screen object (with I2C interface)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);		// Creating screen object (with I2C interface)
 
 
 // DHT sensor confiration
-#define DHTPIN 17							// Data pin I choose
-#define DHTTYPE DHT11				// DHT type
+#define DHT_PIN 17							// DHT data pin (choosen)
+#define DHT_TYPE DHT11				// DHT type
 
-DHT dht(DHTPIN, DHTTYPE);		// Creating DHT object
+DHT dht(DHT_PIN, DHT_TYPE);		// Creating DHT object
+
+
+// LDR sensor configuration
+#define LDR_PIN 27						   // LDR data pin (choosen)
+#define LDR_MAX_VALUE 4096  // LDR max data (observed, not found in doc)
 
 
 void setup() {
 	// Screen setup
-	Serial.begin(115200);			// Screen framerate ?
+	Wire.begin();
+	Serial.begin(115200);			// Monitor refresh rate
 
 	// Screen initialization
 	if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
@@ -41,33 +48,47 @@ void setup() {
 
 	// DHT setup
 	dht.begin();
+
+
+	// LDR setup
+	analogReadResolution(12); // Resolution over 12 bits (0-4095)
 }
 
 
 void loop() {
-	delay(2000);			// DHT sensor refresh rate
-
+	
 	float temperature = dht.readTemperature();		// in °C
 	float humidity = dht.readHumidity();       				 // in %
 
-	// Check every record validity
+	// Check every DHT record validity
 	if (isnan(temperature) || isnan(humidity)) {
-    	Serial.println("Erreur de lecture !");
+		Serial.println("Erreur de lecture !");
     	return;
 	}
 
-	// Display on the monitor tab 
-	Serial.print("Temperature: ");
-	Serial.print(temperature);
-	Serial.print(" °C | Humidity: ");
-	Serial.print(humidity);
-	Serial.println(" %");
-
+	// LDR data read
+	int ldrValue = LDR_MAX_VALUE - analogRead(LDR_PIN); 
+	int ldrPercent = map(ldrValue, 0, LDR_MAX_VALUE, 0, 100);
+	
+	// // Display on the monitor tab 
+	if(Serial) {
+		Serial.print("Temperature: ");
+		Serial.print(temperature);
+		Serial.print(" °C | Humidity: ");
+		Serial.print(humidity);
+		Serial.println(" % | LDR value: ");
+		Serial.println(ldrPercent);
+	}
+	
 	// Display on screen
 	display.clearDisplay();
-	display.setCursor(0, 16);
+	display.setCursor(0, 0);
 	display.printf("Tem: %.1fC", temperature);
-	display.setCursor(0, 42);
+	display.setCursor(0, 20);
 	display.printf("Hum: %.1f%%", humidity);
+	display.setCursor(0, 40);
+	display.printf("Lum: %.1d%%", ldrPercent);
 	display.display();
+	
+	delay(2000);			// DHT sensor refresh rate
 }
